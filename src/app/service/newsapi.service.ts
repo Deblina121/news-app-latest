0db38253e030254f06d363bec6d11d0539107fec
Deblina121 <deblinas146@gmail.com>
2024-08-09 10:20:22 +0530

    first

--- src/app/service/newsapi.service.ts
+++ src/app/service/newsapi.service.ts
@@ -9,9 +9,8 @@ import { Observable } from 'rxjs';
 export class NewsapiService {
   constructor(private _http: HttpClient) { }
 
-  topNews='https://newsapi.org/v2/top-headlines?country=in&apiKey=c78c0eab92504ed2a74ce7102cf494a1';
-
-  techNews='https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=c78c0eab92504ed2a74ce7102cf494a1'
+  topNews='https://newsapi.org/v2/top-headlines?country=in&apiKey=7cbacda1d0804a5293c0253a2f5554d3';
+  techNews='https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=7cbacda1d0804a5293c0253a2f5554d3';
   
   topHeadlines(): Observable<any>{
     return this._http.get(this.topNews);
